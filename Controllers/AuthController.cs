4bea7211c1fc903bba82df525f5ec6bbd51a7e24
Aditya Soni <adityasoni01199@gmail.com>
2025-03-29 22:49:59 +0530

    Updated Backend with authentication

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using MyAPIProject.Models;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
---
+++ Migrations/20250306203413_added UserRequeststbl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAPIProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedUserRequeststbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
